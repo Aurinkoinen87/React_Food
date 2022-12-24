7fbe6f3a1293e14f3b80b7a82008c3cfc72793e6
Aurinkoinen87 <60996322+Aurinkoinen87@users.noreply.github.com>
2022-12-24 03:18:44 +0300

    backend in progress 2

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,25 @@ import cors from 'cors'
 import fs from 'fs'
 import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import UserModel from './models/User.js'
 import { authValidation } from './validations/auth.js'
 import { validationResult } from 'express-validator'
 
 
-mongoose.connect('mongodb+srv://alebastr:BC1PmPNUYwylSBBQ@davaidavai.q6aq7i0.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://alebastr:RooXVntj81UbqpIK@cluster0.cjwko2o.mongodb.net/food?retryWrites=true&w=majority')
+.then(()=> console.log('DB connected')
+)
+.catch((err)=> console.log(err)
+)
 
 const app = express()
 
 
 const PORT = 7000
 
-
-
-
 app.use(cors())
-
+app.use(express.json())
 
 
 app.listen(PORT, (error)=> {
@@ -44,12 +47,31 @@ app.get('/', (req, res)=> {
     
 })
 
-app.post('/auth/register', authValidation, (req, res)=> {
-  const errors = validationResult(req)
-  if(!errors.isEmpty) return res.status(400).json(errors.array())
-  res.json({
-    success: true,
-  })
+app.post('/auth/register', authValidation, async (req, res)=> {
+
+  try {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) return res.status(400).json(errors.array())
+  
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(req.body.password, salt)
+  
+    const doc = new UserModel({ 
+      name: req.body.name,
+      email: req.body.email,
+      password: passwordHash,
+    })
+  
+    const user = await doc.save()
+  
+    res.json({...user._doc})
+  } 
+  catch(err) {
+    console.log(err)   
+    res.status(500).json({
+      message: 'registration was unsuccessful'
+    })
+  }
 })
 
 
